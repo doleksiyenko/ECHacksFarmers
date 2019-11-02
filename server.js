f4493102a8a91c30508a3fc7d4c16c40fd2cba22
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2019-11-02 13:18:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
+
+const DARK_SKY_API_KEY = process.env.DARK_SKY_API_KEY
+const express = require("express")
+
+const app = express()
+app.use(express.json)
+app.use(express.static('public'))
+
+app.post('\weather', (req, res) =>{
+
+})
+
+app.listen(3000, () => {
+    console.log("Server Running")
+})
