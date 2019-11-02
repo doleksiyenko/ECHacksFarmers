b26acca6c8856116193401c96718a273f13b223e
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2019-11-02 18:47:09 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -19,3 +19,24 @@ searchBox.addListener('places_changed', () => {
     setWeatherData(data, place.formatted_address)
   })
 })
+
+const icon = new Skycons({color: '#222'})
+const locationElement = document.querySelector('[data-status]')
+const windElement = document.querySelector('[data-wind]')
+const humidityElement = document.querySelector('[data-humidity]')
+const dewElement = document.querySelector('[data-dew]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const bearingElement = document.querySelector('[data-bearing]')
+
+function setWeatherData(data, place) {
+  locationElement.textContent = data.summary
+  temperatureElement.textContent = data.temperature
+  windElement.textContent = data.windSpeed
+  bearingElement.textContent = data.windBearing
+  dewElement.textContent = data.dewPoint
+  precipitationElement.textContent = data.precipProbability
+  icon.set('icon', data.icon)
+  icon.play()
+}
+
